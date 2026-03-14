44362a4494be0ebe48bb6bc5be10a7c79ed81add
zhangsgithub04 <ssen_zhang@hotmail.com>
2026-03-14 13:42:42 -0400

    Add new allowed origin for CORS

--- backend/standardserver.py
+++ backend/standardserver.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://dtrefstandard-ui.onrender.com",
         "https://zhangsgithub04.github.io",
     ],
     allow_credentials=False,
